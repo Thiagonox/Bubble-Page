90caf2aaa98136ab0c2d7fb4a75ada9aa99a607c
Thiagonox <thiagogentil499@gmail.com>
2023-01-21 13:39:01 -0300

    responsividade

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,14 @@
 <template>
     <div>
-        <div  class="min-h-screen grid md:grid-cols-2 md:grid-rows-6 bg-bg">
+        <div  class="min-h-screen md:grid md:grid-cols-2 md:grid-rows-6 bg-bg">
             <div class="rounded-3xl px-4 pb-4 flex flex-col justify-between md:m-1 col-span-1 row-span-6  backdrop-blur-xl">
-                <header class="w-full h-[16%] flex items-center justify-between">
+                <header class="w-full h-[16%] flex items-center justify-between mb-12">
                     <span class="text-5xl text-txt-primary">Under</span>
                     <button class="w-28 h-10 rounded-3xl bg-[#000] text-[#FFF]">Pedidos</button>
                 </header>
                 <div class="text-txt-primary">
-                    <h2 class="text-8xl mb-2">Streetwear</h2>
-                    <h2 class="text-8xl mb-2">Подземный</h2>
+                    <h2 class="text-7xl md:text-8xl mb-2">Streetwear</h2>
+                    <h2 class="text-7xl md:text-8xl mb-2">Подземный</h2>
                     <p>Welcome to our underground streetwear store. Unique and different pieces that stand out. We believe in personal expression and standing out. Created by indie designers, made with quality. If you're looking for something authentic, you're in the right place. Check out our collection and find your underground streetwear style.</p>
                 </div>
             </div>
@@ -16,7 +16,7 @@
                 <button class="p-4 h-10 flex items-center rounded-3xl border-[#1c2331] border-2">A História</button>
                 <button class="p-4 h-10 flex items-center rounded-3xl border-[#1c2331] border-2">Encomendas</button>
                 <button class="p-4 h-10 flex items-center rounded-3xl border-[#1c2331] border-2">Promoções Secretas</button>
-                <button class="p-4 h-10 flex items-center rounded-3xl border-[#1c2331] border-2">A História</button>
+                <button class="p-4 h-10 flex items-center rounded-3xl border-[#1c2331] border-2">Contato</button>
             </div>
             <div class="rounded-3xl md:m-1 overflow-hidden  col-span-1 row-span-5">
                 <img class="w-full h-full" src="https://photo-cdn2.icons8.com/vyVf2D299YKG9unr7OPmUoyszIkEOGHMuTovvmTfvrM/rs:fit:1612:1072/czM6Ly9pY29uczgu/bW9vc2UtcHJvZC5l/eHRlcm5hbC9hMmE0/Mi8xZDEyOTFjYjEy/MzY0Y2EwOGUwNzgy/Yjk5ZjAyOGI3MC5q/cGc.jpg" alt="">
