8552b3720b981f9007ca5b400d4c8cf94a5d7268
Thiagonox <thiagogentil499@gmail.com>
2023-01-20 22:06:21 -0300

    feat: layout grid

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div  class="h-screen grid md:grid-cols-2 md:grid-rows-6">
+        <div class="rounded-3xl md:m-1 col-span-1 row-span-6 bg-blue-700">
+            
+        </div>
+        <div class="rounded-3xl md:m-1 col-span-1 row-span-1 bg-gray-600">
+
+        </div>
+        <div class="rounded-3xl md:m-1 col-span-1 row-span-5 bg-red-700">
+
+        </div>
+    </div>
+</template>
